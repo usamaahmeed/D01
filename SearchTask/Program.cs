ad47899257ce305ee01b89ae360459f11a48370d
osama ahmed <ososah2001@gmail.com>
2024-11-03 00:48:48 +0200

    Add project files.

---
+++ SearchTask/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SearchTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1000;
+            int y = 2000;
+            Console.WriteLine($"Total: {x} + {y} = {x + y:N}");
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallPrice = 25;
+            int LargePrice = 35;
+            double taxRate = .06;
+            int numOfSmallCarpet;
+            int numOfLargeCarpet;
+            int totalCost;
+            double taxCost;
+
+            Console.Write("Number of small carpets is : $");
+            numOfSmallCarpet = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number of Large carpets is : $");
+            numOfLargeCarpet = Convert.ToInt32(Console.ReadLine());
+
+            totalCost = smallPrice * numOfSmallCarpet + LargePrice * numOfLargeCarpet;
+
+            taxCost = totalCost * taxRate;
+
+            Console.WriteLine($"Cost : ${totalCost} ");
+            Console.WriteLine($"Tax : ${taxCost} ");
+            Console.WriteLine("=====================");
+            Console.WriteLine($"Total estimate: ${taxCost + totalCost}");
+            Console.WriteLine("This estimate is valid for 30 days");
+
+        }
+    }
+}
